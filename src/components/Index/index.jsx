10da72b17d58ae7afe203b7ad0f61e8b8ea52f95
Gian Chiariello <gchiariello@pentass.com>
2022-11-15 22:13:00 -0300

    Boxs images

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -7,6 +7,24 @@ const Index = () => {
       <div className="cart">Carrito</div>
       <div className="black-line">Linea Negra</div>
       <div className="search-bar">Buscador</div>
+
+      <div className="Container">
+        <div className="Boxs Boxs1">
+          <div className="Box">
+            <span>Lorem Ipsum</span>
+          </div>
+        </div>
+        <div className="Boxs Boxs2">
+          <div className="Box">
+            <span>Lorem Ipsum</span>
+          </div>
+        </div>
+        <div className="Boxs Boxs3">
+          <div className="Box">
+            <span>Lorem Ipsum</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
