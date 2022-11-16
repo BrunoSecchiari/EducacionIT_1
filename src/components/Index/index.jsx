d4c0a538250fbce751697dcf999ee31364508735
Bruno Secchiari <bsecchia@ar.ibm.com>
2022-11-15 22:24:46 -0300

    CSS

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -3,10 +3,12 @@ import "./index.css";
 const Index = () => {
   return (
     <>
-      <h3 className="header">MioMio</h3>
-      <div className="cart">Carrito</div>
-      <div className="black-line">Linea Negra</div>
-      <div className="search-bar">Buscador</div>
+      <div className="header">
+        <h3 className="title">MioMio</h3>
+        <div className="cart">Carrito</div>
+        <div className="black-line"></div>
+      </div>
+      <div className="search-bar">Tu búsqueda...</div>
     </>
   );
 };
